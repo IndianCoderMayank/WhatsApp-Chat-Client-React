add1f3162d6a56fe1560bd4e8a03a61d048b9e76
vBOSS.Tech <vbosstech@gmail.com>
2019-03-20 19:59:34 +0700

    Stable Version 0.1.5 NEW

--- src/components/ChatRoomScreen/index.tsx
+++ src/components/ChatRoomScreen/index.tsx
@@ -8,7 +8,7 @@ import MessageBox from './MessageBox'
 import MessagesList from './MessagesList'
 
 const Style = styled.div`
-  .ChatRoomScreen-body {
+  .ChatScreen-body {
     position: relative;
     background: url(/assets/chat-background.jpg);
     width: 100%;
@@ -37,13 +37,13 @@ export default ({ match, history }: RouteComponentProps) => {
   const chatId = match.params.chatId
 
   return (
-    <Style className="ChatRoomScreen Screen">
+    <Style className="ChatScreen Screen">
       <Navbar>
         <Suspense fallback={null}>
           <ChatNavbar chatId={chatId} history={history} />
         </Suspense>
       </Navbar>
-      <div className="ChatRoomScreen-body">
+      <div className="ChatScreen-body">
         <Suspense fallback={null}>
           <MessagesList chatId={chatId} />
         </Suspense>
